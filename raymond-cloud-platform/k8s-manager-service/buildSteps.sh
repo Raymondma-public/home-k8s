302e1482f445620842e3ade88f857a8d9d9ffd67
Raymondma-public <raymondma.public@gmail.com>
2020-07-05 10:18:16 -0400

    [Bugfix] Make buildVM not show error

--- raymond-cloud-platform/k8s-manager-service/buildSteps.sh
+++ raymond-cloud-platform/k8s-manager-service/buildSteps.sh
@@ -1,5 +1,7 @@
+version=$1
+
 ./gradlew build -Dspring.profiles.active=prod
-docker build -t k8s-manager-service .
-docker tag k8s-manager-service raymondmapublic/k8s-manager-service
-docker push raymondmapublic/k8s-manager-service
+docker build -t k8s-manager-service:v$version .
+docker tag k8s-manager-service:v$version raymondmapublic/k8s-manager-service:v$version
+docker push raymondmapublic/k8s-manager-service:v$version
 kubectl apply -f k8s-web-manager.yaml
